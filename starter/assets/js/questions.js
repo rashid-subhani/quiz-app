92e0f41cd8a8df68d26e00c37849141d4b7e9f6c
Rashid Subhani <ras_sub2003@yahoo.com>
2023-12-25 22:02:55 +0000

    added questions for the quiz app

---
+++ starter/assets/js/questions.js
@@ -0,0 +1,37 @@
+// first we need to have questions as array of objects
+
+let questions = [
+    {
+      question: "What is the role of the 'DOCTYPE' declaration in an HTML document?",
+      options : ["Specify the HTML version", "Define the document structure", "Declare document styles","Indicate the document title"],
+      correctAnswer: "Specify the HTML version"
+    },
+    {
+        question : "Which of the following is NOT a valid variable name in JavaScript?",
+        options : ["myVar","_variable","123variable","$var"],
+        correctAnswer: "123variable"
+    },
+    {
+        question: "What is the purpose of the 'git clone' command in Git?",
+        options : ["Create a new Git repository","Copy files from one branch to another","Clone a remote repository to the local machine","Delete a Git repository"],
+        correctAnswer: "Clone a remote repository to the local machine"
+    },
+    {
+        question: "What does the acronym 'API' stand for in programming?",
+        options: ["Advanced Programming Interface","Application Programming Interface","Advanced Python Interpreter"," Application Process Integration"],
+        correctAnswer: "Application Programming Interface"
+    },
+    {
+        question: "Which of the following is a dynamically-typed programming language?",
+        options:["Java","C++","Python","TypeScript"],
+        correctAnswer: "Python"
+    }
+]
+
+
+
+
+
+
+Answer:
+3. 
